dceaedf67c02612786399cb1ef06bf163a1fd4b8
Kevin Miller <kevinmiller@Kevins-MacBook-Pro.local>
2019-02-20 18:30:29 -0600

    Finished making everything work correctly. i could problably do some cleaning up and some extra styling

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,30 +1,10 @@
-/* 3. Here's how the app works:
-
-   * There will be four crystals displayed as buttons on the page.
-
-   * The player will be shown a random number at the start of the game.
-
-   * When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
-
-     * Your game will hide this amount until the player clicks a crystal.
-     * When they do click one, update the player's score counter.
-
-   * The player wins if their total score matches the random number from the beginning of the game.
-
-   * The player loses if their score goes above the random number.
-
-   * The game restarts whenever the player wins or loses.
-
-     * When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
-
-   * The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
+/*
 
 ##### Option 1 Game design notes
 
 * The random number shown at the start of the game should be between 19 - 120.
 
 * Each crystal should have a random hidden value between 1 - 12.
-*/
 
 //set up vars
 
@@ -38,7 +18,6 @@
 
 // Initial variables
 
-/* 
 //Found a way to test if Jquery was attached
 
       window.onload = function()
@@ -53,6 +32,7 @@
           }
 }
 */
+
 //Load page first
 $(document).ready(function () {
 
@@ -102,10 +82,10 @@ $(document).ready(function () {
     sum.length = 0;
     totalScoreDOM.text("Score " + "0");
     genCrystalValues();
-    console.log(greenValue, redValue, blueValue, yellowValue);
+    //console.log(greenValue, redValue, blueValue, yellowValue);
     genComputerScore();
-    console.log(computerScore);
-    console.log(totalScore);
+    //console.log(computerScore);
+    //console.log(totalScore);
   }
 
   //adds value to wins || losses 
@@ -113,13 +93,13 @@ $(document).ready(function () {
     if (sum === computerScore) {
     wins++;
     winsDOM.text(wins);
-    console.log("wins=" + wins);
+    //console.log("wins=" + wins);
     gameStart();
   }
         else if (sum > computerScore) {
           losses++;
           lossesDOM.text(losses);
-    console.log("losses=" + losses);
+    //console.log("losses=" + losses);
           gameStart();
         }
         
@@ -132,39 +112,39 @@ $(document).ready(function () {
 
 
   $(".greenmateria").on("click", function () {
-    console.log(greenValue);
+    //console.log(greenValue);
     totalScore.push(greenValue);
     userScore();
-    console.log("total " + sum);
+    //console.log("total " + sum);
     $("#totalscore").text("Score " + sum);
     winLoss();
   });
 
   $(".redmateria").on("click", function () {
-    console.log(redValue);
+    //console.log(redValue);
     totalScore.push(redValue);
     userScore();
-    console.log("total " + sum);
+    //console.log("total " + sum);
     $("#totalscore").text("Score " + sum);
     winLoss();
 
   });
 
   $(".bluemateria").on("click", function () {
-    console.log(blueValue);
+    //console.log(blueValue);
     totalScore.push(blueValue);
     userScore();
-    console.log("total " + sum);
+    //console.log("total " + sum);
     $("#totalscore").text("Score " + sum);
     winLoss();
 
   });
 
   $(".yellowmateria").on("click", function () {
-    console.log(yellowValue);
+    //console.log(yellowValue);
     totalScore.push(yellowValue);
     userScore();
-    console.log("total " + sum);
+    //console.log("total " + sum);
     $("#totalscore").text("Score " + sum);
     winLoss();
 
